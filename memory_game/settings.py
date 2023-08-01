76a328ad7d1012b823c4a206468308eb3ae783c1
Maripandi P <maripandi422@outlook.com>
2023-08-01 16:42:54 +0530

    second commit

--- memory_game/settings.py
+++ memory_game/settings.py
@@ -80,12 +80,9 @@ WSGI_APPLICATION = 'memory_game.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':  'memory_game',
-        'USER': 'root',
-        'PASSWORD':'systems',
-        'HOST':'localhost',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  BASE_DIR/'db.sqlite3',
+       
     }
 }
 
